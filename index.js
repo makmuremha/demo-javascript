f3411dd592d85f932f8ffbe95a417459375e3fd6
Makmur Emha <makmur.emha@gmail.com>
2022-09-22 16:20:09 +0800

    fix : enter validation type data

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
+console.log("Enter min number and max number on isPrime function");
 const isPrime = (minNumber, maxNumber) => {
-	let primeNumber = [];
-	for (let number = minNumber; number <= maxNumber; number++) {
-		let diff = 0;
+	if (typeof minNumber != "number" || typeof maxNumber != "number"){
+		return console.log("Please enter a number");
+	}
 
-		for (let i = 1; i <= number; i++) {
-			if (number % i == 0) {
-				diff++;
+	if(minNumber < maxNumber) {
+		let primeNumber = [];
+		for (let number = minNumber; number <= maxNumber; number++) {
+			let diff = 0;
+	
+			for (let i = 1; i <= number; i++) {
+				if (number % i == 0) {
+					diff++;
+				}
+			}
+			
+			if (diff === 2) {
+				primeNumber += `${number},`;
 			}
 		}
-		
-		if (diff === 2) {
-			primeNumber += `${number},`;
-		}
-	}
-	console.log("primes number is "+primeNumber);
+		console.log(`primes number between ${minNumber} and ${maxNumber} is ${primeNumber}`);
+	}else{
+		console.log("min number must be less than max number");
+	}	
 };
-
 isPrime(1,10);
